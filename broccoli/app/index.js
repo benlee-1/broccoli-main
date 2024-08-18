bb5e459f8ea1b186d5373594b2dc8f51f9856ada
benlee-2 <yeongbin.ben.lee@gmail.com>
2024-08-17 19:17:48 -0500

    set up site to render

---
+++ broccoli/app/index.js
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Page from "./page";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Page />);
